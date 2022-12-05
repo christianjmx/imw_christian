8a7fa4d94cb94a8e27d2451e41ec426691ee42e8
christianjmx <73601635+christianjmx@users.noreply.github.com>
2022-12-05 22:54:37 +0000

    Update program2.py

--- trim1/ut2/Secuenciando/program2.py
+++ trim1/ut2/Secuenciando/program2.py
@@ -12,11 +12,11 @@ sequence = ''.join([random.choice(NUCLEOBASES) for i in range(DNA_SIZE)])
 for n in sequence:
     if n =='A':
         a+=1
-    elif n =='G':
+         elif n =='G':
         g+=1
-    elif n =='C':
+         elif n =='C':
         c+=1
-    elif n =='T':
+         elif n =='T':
         t+=1
 
 print(f'''
