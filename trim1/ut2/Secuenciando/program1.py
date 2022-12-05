5dc088ab4505055ad15282589451146dab13307b
christianjmx <73601635+christianjmx@users.noreply.github.com>
2022-12-05 22:54:08 +0000

    Update program1.py

--- trim1/ut2/Secuenciando/program1.py
+++ trim1/ut2/Secuenciando/program1.py
@@ -1,7 +1,7 @@
 import sys
 
-dni=int(sys.argv[1])
-palabra='TRWAGMYFPDXBNJZSQVHLCKE'
-letra= dni%23
+  dni=int(sys.argv[1])
+  palabra='TRWAGMYFPDXBNJZSQVHLCKE'
+  letra= dni%23
 
 print ("El DNI completo es: "f'{dni}{palabra[letra]}')
